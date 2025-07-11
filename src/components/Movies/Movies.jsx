3e04f53bf1e7d3e7d83ec03853245d2ab973fa78
Fatiya Labibah <146101042+fatiya17@users.noreply.github.com>
2025-07-11 20:14:45 +0700

    Update Movies.jsx

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -3,27 +3,12 @@ import Movie from "../Movie/Movie";
 import styles from "./Movies.module.css";
 import { Heading } from "../UI/Typography";
 import { useContext } from "react";
-import MovieContext from "../context/MoviesContext"; // import context
+import MoviesContext from "../context/MoviesContext";
 
-function Movies(props) {  // tambahkan props
-  // const movies = data;
-// const { movies, setMovies } = props; 
-// ambil props movies dan setMovies
-const {title= "Latest Movies"} = props;
-const {movies} = useContext(MovieContext); // ambil movies dari context
-  function handleClick() {
-    const movie = {
-      id: "xyz", 
-      title: "Jigsaw",
-      year: 2021, 
-      type: "Movie",
-      poster: "https://picsum.photos/300/400",
-    };
-
-    console.log(movie);
-    // setMovies([...movies, movie]);
-  }
-
+function Movies(props) {
+  const { title = "Latest Movies" } = props;
+  const { movies } = useContext(MoviesContext); 
+  
   return (
     <div>
       <div className={styles.container}>
@@ -34,8 +19,6 @@ const {movies} = useContext(MovieContext); // ambil movies dari context
               return <Movie key={movie.id} movie={movie} />;
             })}
           </div>
-           {/* Element button diberikan event click: onClick */}
-           <button onClick={handleClick}>Add Movie</button>
         </section>
       </div>
     </div>
