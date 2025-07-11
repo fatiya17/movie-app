3e319e1b773ea6ba9e93ce30ea58d628faf55180
Fatiya Labibah <146101042+fatiya17@users.noreply.github.com>
2025-07-11 20:17:30 +0700

    Update endpoint.jsx

--- src/utils/constants/endpoint.jsx
+++ src/utils/constants/endpoint.jsx
@@ -1,9 +1,13 @@
-const API_KEY = import.meta.env.VITE_API_KEY;   
+// src/utils/constants/endpoint.js
+const API_KEY = import.meta.env.VITE_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
 const ENDPOINTS = {
-    POPULAR:`${BASE_URL}/movie/popular?api_key=${API_KEY}`, 
-    TRENDING:`${BASE_URL}/movie/popular?api_key=${API_KEY}`, 
-}
+  POPULAR: `${BASE_URL}/movie/popular?api_key=${API_KEY}`,
+  NOW_PLAYING: `${BASE_URL}/movie/now_playing?api_key=${API_KEY}`,
+  TOP_RATED: `${BASE_URL}/movie/top_rated?api_key=${API_KEY}`,
+  SEARCH: `${BASE_URL}/search/movie?api_key=${API_KEY}`,
+  DETAIL: (id) => `${BASE_URL}/movie/${id}?api_key=${API_KEY}`
+};
 
 export default ENDPOINTS;
